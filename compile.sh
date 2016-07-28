a5a139c83300a265f5c94000dc72760a65ab2d9e
shivaRamdeen <shiva.ramdeen@outlook.com>
2016-07-28 09:48:48 -0400

    Update compile.sh
    outline path needed for application specific compile instructions
    return name of final binary

--- compile.sh
+++ compile.sh
@@ -7,7 +7,16 @@ NAME=${NAME%.*}
 export PATH:$PATH:/lp/rumprun/rumprun/bin
 
 #cross compile
+#check if application specific compile instructions are present.
+#if [[ -e /app/$NAME]]; then
+# Run application specific compile script
+#else
+#compile generic
+
 x86_64-rumprun-netbsd-gcc -O $NAME-rumprun $NAME.c
 
 #bake
 rumprun-bake hw_virtio $NAME-rumprun.bin $NAME-rumprun
+
+#return name of final binary
+echo $NAME-rumprun.bin
