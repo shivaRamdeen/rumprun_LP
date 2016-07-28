1b13f320d25f5b1511e653fa23b626995d3bd0b3
shivaRamdeen <shiva.ramdeen@outlook.com>
2016-07-28 16:42:28 -0400

    Update compile.sh
    fix compile error

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 NAME=$1
 NAME=${NAME%.*}
-
+COMPILE_FLAGS=$2
 #path to rumprun tools
 export PATH=$PATH:/lp/rumprun/rumprun/bin
 
@@ -13,10 +13,10 @@ export PATH=$PATH:/lp/rumprun/rumprun/bin
 #else
 #compile generic
 
-x86_64-rumprun-netbsd-gcc -O $NAME-rumprun $NAME.c
+x86_64-rumprun-netbsd-gcc -o "$NAME-rumprun" "$NAME.c" $COMPILE_FLAGS
 
 #bake
-rumprun-bake hw_virtio $NAME-rumprun.bin $NAME-rumprun
+rumprun-bake hw_virtio "$NAME-rumprun.bin" "$NAME-rumprun"
 
 #return name of final binary
 echo $NAME-rumprun.bin
